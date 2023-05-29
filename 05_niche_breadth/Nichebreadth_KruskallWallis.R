b0e3c1d06049350a21edb2527f434cb3a55461f1
edgagnon <46787731+edgagnon@users.noreply.github.com>
2023-05-29 00:01:09 -0400

    Create Nichebreadth_KruskallWallis.R

---
+++ 05_niche_breadth/Nichebreadth_KruskallWallis.R
@@ -0,0 +1,15 @@
+##########################################
+#
+# Script for doing the Kruskall-Wallis test on niche breadth of Solanum species
+#
+# by E. Gagnon, part of Solanum Geophyte publication 2023
+#########################################
+
+
+rm(list = ls())
+
+#Opening libraries
+library(tidyverse)
+library(ggpubr)
+library(rstatix)
+
