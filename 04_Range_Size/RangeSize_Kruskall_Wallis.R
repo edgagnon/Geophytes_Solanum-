e13b7fbfe69d494f191b31482eefaf6ede39a93a
edgagnon <46787731+edgagnon@users.noreply.github.com>
2023-05-28 23:24:28 -0400

    Update RangeSize_Kruskall_Wallis.R

--- 04_Range_Size/RangeSize_Kruskall_Wallis.R
+++ 04_Range_Size/RangeSize_Kruskall_Wallis.R
@@ -1,1 +1,10 @@
+##########################################
+#
+# Script for doing the Kruskall-Wallis test on range sizes of Solanum species
+#
+# by E. Gagnon, part of Solanum Geophyte publication 2023
+#########################################
+rm(list = ls())
+#Change working directory
+setwd("C://Users/edeli/OneDrive/Projets_Recherche/2019_Tuber_project/new_TUber_coding/Solanum_geophytes/04_Range_Size_Niche_Breadth/")
 
